4f5f51375d703546a7e6e8187d1ddbd1388a95d1
André Pettersson <andre20030417@gmail.com>
2025-10-30 08:41:16 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PremierLeagueManager
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
