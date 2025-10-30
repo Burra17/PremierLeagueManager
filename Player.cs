2fd2f3afdd94983c884e0a8e886ea0745c0b9ef6
André Pettersson <andre20030417@gmail.com>
2025-10-30 08:54:09 +0100

    Attribut för Spelare

--- Player.cs
+++ Player.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace PremierLeagueManager
 {
-    internal class Player
+    public class Player // Representerar en spelare i Premier League
     {
+        public string Name { get; set; } // Spelarens namn
+        public string Position { get; set; } // Spelarens position på planen
+        public int GoalsScored { get; set; } // Antal mål som spelaren har gjort
+        public int Assists { get; set; } // Antal assist som spelaren har gjort
+        public string Team { get; set; } // Namnet på spelarens lag
+        public int Rating { get; set; } // Spelarens betyg baserat på prestationer
+
+        // Konstruktor för att initiera en spelare med nödvändig information
+        public Player(string name, string position, int goalsScored, int assists, string team, int rating)
+        {
+            Name = name;
+            Position = position;
+            GoalsScored = goalsScored;
+            Assists = assists;
+            Team = team;
+            Rating = rating;
+        }
     }
 }
